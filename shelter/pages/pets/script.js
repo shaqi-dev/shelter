3ea26d954e2a8310c4e6fb07d57440afffd1ede3
Vladislav Zubakin <shaqimusic@gmail.com>
2022-04-18 15:35:17 +0300

    fix: bug that stopped cards rendering

--- shelter/pages/pets/script.js
+++ shelter/pages/pets/script.js
@@ -2,28 +2,26 @@
 
 import PetsService from "../../pets-service.js";
 
-// Render pets cards
-
-const petsService = new PetsService();
-      pets = await petsService.getAllPets(),
-      petCards = pets.map(pet => petsService.createPetCard(pet));
-
-const cardsWrapper = document.querySelector('.our-friends__cards')
-
-petCards.forEach(card => {
-    cardsWrapper.append(card)
-})
-
 // Disable last 2 navigation buttons
 
 const navigationItems = document.querySelectorAll('.navigation__item')
 
-for (let i = 0; i < navigationItems.length; i++) {
-    const lastItemsCount = 2
-        
-    if (i >= navigationItems.length - lastItemsCount) {
-        navigationItems[i].addEventListener('click', e => {
-            e.preventDefault();
-        })
-    }
+function disableClickEvent(element) {
+    element.addEventListener('click', e => e.preventDefault());
 }
+
+disableClickEvent(navigationItems[2]);
+disableClickEvent(navigationItems[3]);
+
+// Render pets cards
+
+const petsService = new PetsService(),
+      pets = await petsService.getAllPets(),
+      petCards = pets.map(pet => petsService.createPetCard(pet));
+
+const cardsWrapper = document.querySelector('.our-friends__cards');
+
+petCards.forEach(card => {
+    cardsWrapper.append(card)
+});
+
